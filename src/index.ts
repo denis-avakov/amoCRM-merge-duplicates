e62d3293713135097bf7b9086ba55baa6ebec024
Denis Avakov <56247240+denis-avakov@users.noreply.github.com>
2024-12-11 10:20:39 +0200

    feat: simplify docker image

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('hey');
+const PORT = process.env.PORT || 3000;
+
+Bun.serve({
+  port: PORT,
+  fetch(req) {
+    const url = new URL(req.url);
+
+    if (url.pathname === '/') {
+      return new Response('Hey!');
+    }
+
+    return new Response('404!');
+  },
+});
+
+console.log(`Server running at http://localhost:${PORT}`);
